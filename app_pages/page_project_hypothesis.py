4bf8b5f8c476537521eb579e8505ba84d593504d
Amare Teklay Hailu <amareteklay@gmail.com>
2022-10-15 11:43:28 +0000

    Edit hypotheses and validation page

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -6,10 +6,18 @@ def page_project_hypothesis_body():
 
     # conclusions taken from "03 - Correlation_Study" notebook 
     st.success(
-       f"* Size matters. Variables that are associated with the size of the house are positively correlated to sale price. We will examine correlations between attributes about the size of the house and the sale price.\n\n"
+       f"**H1 - Size matters.** We hypothesize that larger the property, the higher its sale price will be.\n"
+        f"* **Correct.** From the correlation study results, we found that features that reflect the size of\
+             a property were positively and moderately correlated with sale price.\n\n"
        
-       f"* Ratings of the quality and condition of the house reflect its value and thus higher ratings indicate higher sale price. We will use the correlation between variables about the different ratings of the house and the sale price to validate this hypothesis.\n\n"
+       f"**H2 - Quality matters.** Ratings of the quality and condition of the house would reflect its value,\
+         and thus we suspect that higher quality ratings indicate higher sale price.\n"
+        f"* **Correct.**  We used the correlation between sale price and the kitchen quality and overall quality\
+             ratings to show that this is indeed the case.\n\n"
        
-       f"* Age of the house is expected to have significant influence on the sale price of the house. We study when the house was built and how expensive it is to test this hypothesis.\n"
+       f"**H3 - Time matters.** We expect that the value of a property will be significantly influenced by how\
+         old the property is and/or whether it had any remodel added to it recently.\n"
+        f"* **Correct.** We validated this hypothesis by studying the correlation between the sale price and\
+             the years it was built and/or had a remodel added to it. Both features have moderate positive correlation with sale price.\n"
        )
         
