a79dc01de8240879e2f419dddf492b5819db92d1
Amare Teklay Hailu <amareteklay@gmail.com>
2022-10-15 13:15:59 +0000

    ML page: edit textual interpretations

--- app_pages/page_ML_predict_price.py
+++ app_pages/page_ML_predict_price.py
@@ -24,14 +24,17 @@ def page_ml_predict_price_body():
     st.write("### ML Pipeline: Predict House Price")    
     # display pipeline training summary conclusions
     st.info(
-        f"* The pipeline was tuned aiming at least 0.75 accuracy for the sales price of a property with a given set of attributes.\n"
-
-        f"* The pipeline performance on the train and test set is 0.99 and 0.79 respectively.\n"
+        f"* To answer BR2, we wanted to train a Regressor model and tune the pipeline aiming to ensure\
+             at least 0.75 accuracy in predicting the sales price of a property with a given set of\
+                 attributes. We were able to achieve this success metric, but we trained different\
+                 versions of the model to make sure that we don't pverlook potential improvement.\n"
+        f"* The pipeline performance for the best model on the train and test set is\
+             R2 == 0.99 and R2 == 0.79 respectively.\n"
+        f"* We present the pipeline steps, best features list along with feature importance plot, pipeline performance and regression performance report below. "
        )
     st.write("---")
 
     # show pipeline steps
-    st.write("* ML pipeline to clean the data ")
     st.write("* ML pipeline to predict sales prices of houses ")
     st.code(price_pipe)
     st.write("---")
@@ -44,9 +47,15 @@ def page_ml_predict_price_body():
 
 
     st.write("### Pipeline Performance")
+    st.write("##### Performance goal of the predictions:\n")
+    st.write("* We agreed with the client an R2 score of at least 0.75 on the train set as well as on the test set.")
+    st.write(f"* Our ML pipeline performance shows that our model performance\
+         metrics have been successfully satisfied.")
     regression_performance(X_train=X_train, y_train=y_train,
                         X_test=X_test, y_test=y_test,
                         pipeline=price_pipe) 
 
     st.write("### Regression Performance Plots")
+    st.write("* The regression performance plots below indicate that our model,\
+         in most part, is able to predict sale prices well. The model looks less effective for houses with high prices though.")
     regression_evaluation_plots(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, pipeline=price_pipe)  
